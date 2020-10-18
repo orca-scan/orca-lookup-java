70269610f74bbc2cf5e6664c7e0e3bb2e43d47df
John Doherty <contact@johndoherty.info>
2020-10-18 01:43:46 +0100

    Added working example

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,4 +1,4 @@
-package example.smallest;
+package orcalookup.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
--- src/main/java/controllers/LookupController.java
+++ src/main/java/controllers/LookupController.java
@@ -1,5 +1,6 @@
-package example.smallest.controllers;
+package orcalookup.java.controllers;
 
+import java.util.HashMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,7 +10,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LookupController {
 	
 	@RequestMapping(method = RequestMethod.GET, produces = {"application/json"})
-	public @ResponseBody String helloWorld() {
-		return "Hello World"; //"application/json" mean this is a text not a redirect
+	@ResponseBody
+	public HashMap<String, Object> Lookup(String barcode) {
+
+		// TODO: query a database or API to retrieve some data based on barcode value
+
+		// return data as JSON object (property names must match Orca column names)
+		HashMap<String, Object> data = new HashMap<String, Object>();
+
+		data.put("VIN", barcode);
+		data.put("Make", "SUBARU");
+		data.put("Model", "Legacy");
+		data.put("Manufacturer Name", "FUJI HEAVY INDUSTRIES U.S.A");
+		data.put("Vehicle Type", "PASSENGER CAR");
+		data.put("Year", 1992);
+
+    	return data;
 	}
 }
