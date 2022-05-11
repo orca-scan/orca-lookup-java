7ed3d716ccced7d8a00076638bec3077193b170e
wole-kolawole <101117076+wole-kolawole@users.noreply.github.com>
2022-05-12 00:46:32 +0100

    Fixed springframework version number, SpringBootServletInitializer package location and mvn -D command line argument

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,7 +2,7 @@ package orcalookup.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
